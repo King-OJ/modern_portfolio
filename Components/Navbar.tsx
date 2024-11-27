a64f4a4c73c28f088716627df1662630ee941bf0
Clement Ojiguo <clementojigs@hotmail.com>
2024-11-27 05:21:58 -0800

    added recent worsk UI

---
+++ Components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import ToggleTheme from "./ToggleTheme";
+
+import { Menubar, MenubarMenu, MenubarTrigger } from "./ui/menubar";
+
+function Navbar() {
+  const { theme } = useTheme();
+  const links: Array<string> = ["about", "portfolio", "contact"];
+  return (
+    <div className="flex items-center h-12">
+      <div className="rounded-full border-2 p-1 h-full">
+        <Image
+          className="w-auto h-full"
+          src={
+            theme == "dark" ? "/darkmode_logo_small.png" : "/lightmode_logo.png"
+          }
+          alt="Clement Ojiguo logo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          priority
+        />
+      </div>
+      <nav className="flex-1 h-full mr-6">
+        <Menubar className="h-full rounded-full justify-between bg-muted px-12">
+          {links.map((link, index) => {
+            return (
+              <MenubarMenu key={index}>
+                <MenubarTrigger className="uppercase">{link}</MenubarTrigger>
+              </MenubarMenu>
+            );
+          })}
+        </Menubar>
+      </nav>
+
+      <ToggleTheme />
+    </div>
+  );
+}
+export default Navbar;
---
+++ Components/RecentWorks.tsx
@@ -0,0 +1,61 @@
+import { Badge } from "./ui/badge";
+
+function RecentWorks() {
+  type WorkProps = {
+    company: string;
+    role: string;
+    startDate: Date;
+    endDate?: Date;
+  };
+
+  const date = Date.now();
+
+  const works: Array<WorkProps> = [
+    {
+      company: "Fiverr",
+      role: "Frontend Developer",
+      startDate: new Date(date),
+    },
+    {
+      company: "Upwork",
+      role: "FullStack Developer",
+      startDate: new Date("Jul 12 2022"),
+    },
+    {
+      company: "LinkedIn",
+      role: "Freelancer Developer",
+      startDate: new Date("Jul 12 2020"),
+    },
+  ];
+
+  return (
+    <section className="bg-muted px-6 py-10 rounded-xl border">
+      <div className="flex items-center space-x-2">
+        <Badge className="p-[2px] bg-accent-foreground" />
+        <h3 className="text-accent-foreground font-semibold">Recent Works</h3>
+      </div>
+
+      <div className="mt-8">
+        <ul className="space-y-4">
+          {works.map((work, index) => {
+            return (
+              <li key={index} className="flex items-center justify-between">
+                <div className="space-y-1">
+                  <h6 className={index == 0 ? "text-primary" : ""}>
+                    {work.role}
+                  </h6>
+                  <p className="text-accent-foreground text-sm">
+                    {work.startDate.getFullYear()} -{" "}
+                    {work.endDate ? work.endDate.getFullYear() : "Present"}
+                  </p>
+                </div>
+                <div className="text-accent-foreground">{work.company}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+}
+export default RecentWorks;
